64d2ee379a7765b121704b82c0d3f273ace75625
uri wald <uriwald@walla.com>
2012-08-14 23:32:28 +0800

    Adding another option for getting the locale parameter

--- localization.php
+++ localization.php
@@ -1,11 +1,15 @@
 <?php
 
     $locale = "en_US.utf8";
-    if (isSet($_GET["locale"])) $locale = $_GET["locale"].".utf8";
+    if (isSet($_GET["locale"])) 
+        $locale = $_GET["locale"].".utf8";
+    if (isSet($_GET["ltranslate"])) 
+        $locale = $_GET["ltranslate"].".utf8";
+    //echo $locale;
     putenv("LC_ALL=$locale");
     setlocale(LC_ALL, $locale);
     //setlocale(LC_ALL, "sp_SP.utf8");
     bindtextdomain("messages", "./locale");
     textdomain("messages");
-
+    
 ?>
