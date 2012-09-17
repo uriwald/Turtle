af55328c72fe9607c8f5f79d9a133cc607ba07b5
uri wald <uriwald@walla.com>
2012-09-17 14:00:21 +0800

    Redirecting back to lessons page

--- approveLesson.php
+++ approveLesson.php
@@ -3,14 +3,12 @@
 /*
  * Approve or unapprove lessons
  */
-        require_once("files/collectionUtil.php");
+        require_once("files/utils/collectionUtil.php");
         $lu = new collectionUtil("turtleTestDb" , "lessons");
-        
-        $lessonFinalTitle = "";
-        if (isset($_GET['lesson'])) {
+                if (isset($_GET['lesson'])) {
             $lesson = new MongoId($_GET['lesson']);
             $bool = $_GET['pending'];
             $collection = $lu->CollectionItemChangeAttribute($lesson,"pending",$bool == 'true'? true: false);
-            echo $collection; 
+            header("location: lessons.php");
         }
 ?>
