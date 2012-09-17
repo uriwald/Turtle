2e2b34ddcc69daa9fa8f2aa89608d7470b5dd4ae
uri wald <uriwald@walla.com>
2012-09-17 13:54:19 +0800

    reactivating the footer link on main page

--- files/footer.php
+++ files/footer.php
@@ -2,17 +2,15 @@
         $currentFile = $_SERVER["PHP_SELF"];
         $parts = Explode('/', $currentFile);
         $currentPage = $parts[count($parts) - 1];
-        
+        $doc = _('project doc');
         $footer =
         "
         <footer id='footer'>
-            &copy; TurtleAcademy, <a id='doc' title='תעוד הפרוייקט' href='doc.html'>
-            <?php
-                 echo _('project doc'');
-            //        תעוד הפרוייקט                     
-             ?> 
-                                 
-            </a>
+            &copy; TurtleAcademy, 
+                <a id='doc' title='תעוד הפרוייקט' href='doc.html'>
+                ". 
+                $doc ."
+                </a>
             <div id='langicons'>
                 <a href=$currentPage?locale=he_IL><img src='Images/flags/il.png'  title='עברית' /></a>
                 <a href=$currentPage> <img src='Images/flags/us.png'  title='English' /></a>              
