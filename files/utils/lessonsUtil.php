6881d50262d64c70d867e48c7be8cb0c480e8731
uri wald <uriwald@walla.com>
2012-09-17 13:46:04 +0800

    Moving util files to other folder

---
+++ files/utils/lessonsUtil.php
@@ -0,0 +1,95 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of lessonsUtil
+ *
+ * @author lucio
+ */
+class lessonsUtil {
+
+    private $locale = "en_US";
+    private $localePrefix = "locale_";
+    private $db;
+    private $lessonObjId;
+    private $steps;
+    private $titles;
+
+    
+    # Constructor  
+    public function __construct($locale,$localePrefix,$db,$lessonObjId)  
+     {  
+                $this->locale = $locale;
+                $this->localePrefix = $localePrefix; 
+                $this->db = $db; 
+                $this->lessonObjId = $lessonObjId;
+                self ::setLessonStepsAndTitles();
+     }  
+
+    public function __get($property) {
+        if (property_exists($this, $property)) {
+            return $this->$property;
+        }
+    }
+
+    public function __set($property, $value) {
+        if (property_exists($this, $property)) {
+            $this->$property = $value;
+        }
+
+        return $this;
+    }
+
+    public function setLessonStepsAndTitles() {
+        $theObjId = new MongoId($this->lessonObjId);
+        $cursor = $this->db->findOne(array("_id" => $theObjId));
+        $this->titles = $cursor["title"];
+        $this->steps = $cursor["steps"];
+    }
+
+    public function printSteps() {
+        print_r($this->steps);
+    }
+    
+    public function getStepsByLocale($locale) {
+        // Here i put default value
+        // I don't think it's the best practise better return null for translation issues
+        $localeSteps = null;
+        if(isset($this->steps))
+        {
+            foreach ($this->steps as $key => $value) {
+                if (isset($this->steps[$key][$locale])) {
+
+                    $localeSteps[$key] = $this->steps[$key][$locale];
+                }
+            }
+        }
+        return $localeSteps;
+    }
+
+    public function getTitleByLocale($locale) {
+        $titleByLocale = "";
+        if(isset($this->titles))
+        {
+            foreach ($this->titles as $key => $value) {
+                if (isset($this->titles[$key])) 
+                {
+                    if ($key == $locale) {
+                        $titleByLocale = $value;
+                    }
+                } 
+                else 
+                { 
+                  //  $this->titles = $this->titles[$key];
+                }
+            }
+        }
+        return $titleByLocale;
+    }
+}
+
+?>
