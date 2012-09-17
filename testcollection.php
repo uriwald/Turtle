5a170b6138dcf0ddf8d21fff0ea8a3d238980387
uri wald <uriwald@walla.com>
2012-09-17 13:53:36 +0800

    Having the procedures for collection testing

--- testcollection.php
+++ testcollection.php
@@ -1,8 +1,10 @@
 <?php
-    require_once("files/collectionUtil.php");
+    require_once("files/utils/collectionUtil.php");
     $lu = new collectionUtil("turtleTestDb" , "lessons");
     $singelItemId = new MongoId("4fec515e267cfe8c0d000000");
-    $collection = $lu->CollectionItemChangeAttribute($singelItemId,"pending",false);
-    //$collection = $lu->printCollectionItems();
+    $collection = $lu->CollectionItemsAddAttribute("pending" , false);
+    //$collection = $lu->CollectionItemChangeAttribute($singelItemId,"pending",false);
+    ///$collection = $lu->printCollectionItems();
     echo $collection;
+    
 ?>
