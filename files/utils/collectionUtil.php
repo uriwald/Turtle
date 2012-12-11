78bb09b96bbe026100bdd6de984241438d973114
uri wald <uriwald@walla.com>
2012-12-11 20:22:30 +0700

    Adding functionality to copy only a single step change according to his locale

--- files/utils/collectionUtil.php
+++ files/utils/collectionUtil.php
@@ -90,8 +90,62 @@ class collectionUtil {
         }
         return $localeSteps;
     }
-
-
+    /*
+     * We assusme the the same MongoID is exist in both of the collection
+     * good for example to move data from lessons to lessons_translate
+     */
+    public static function copyFullLessonBetweenCollections($mongoid ,$dbName,$colFromName , $colToName) {
+        $mo = new Mongo();
+        // select a database
+        $dbb = $mo->$dbName;
+        // select a collection (analogous to a relational database's table)
+        $colFrom    = $dbb->$colFromName;
+        // print_r($colFrom);
+        $colTo      = $dbb->$colToName;
+        $criteria1  = $colFrom->findOne(array("_id" => $mongoid));
+        //print_r($criteria1);
+        $criteria2  = $colTo->findOne(array("_id" => $mongoid));
+        //print_r($criteria2);
+        if (!isset($criteria2))
+            
+        {
+            if (isset($criteria1))
+                $colTo->insert($criteria1);
+        }
+        if (isset($criteria1) && isset($criteria2))
+             $colTo->update($criteria2,$criteria1);
+    }
+    public static function copyLocaleLessonBetweenCollections($mongoid ,$dbName,$colFromName , $colToName , $locale , $stepnum) {
+        $mo = new Mongo();
+        // select a database
+        $dbb = $mo->$dbName;
+        // select a collection (analogous to a relational database's table)
+        $colFrom    = $dbb->$colFromName;
+        // print_r($colFrom);
+        $steps              = "steps";
+        $colTo              = $dbb->$colToName;
+        $criteria1          = $colFrom->findOne(array("_id" => $mongoid));
+        //$criteria1Copy      = $criteria1 ;
+        //print_r($criteria1);
+        print_r ($criteria1[$steps][$stepnum][$locale]);
+        $criteria2          = $colTo->findOne(array("_id" => $mongoid));
+        $criteria2Copy      = $criteria2;
+        $criteria2Copy[$steps][$stepnum][$locale] = $criteria1[$steps][$stepnum][$locale];
+        if (isset ($criteria2[$steps][$stepnum][$locale]))
+             print_r ($criteria2[$steps][$stepnum][$locale]);
+        //print_r($criteria2);
+        /*
+        if (!isset($criteria2))
+            
+        {
+            if (isset($criteria1))
+                //$colTo->insert($criteria1);
+        }
+       // if (isset($criteria1) && isset($criteria2))
+             //$colTo->update($criteria2,$criteria1);
+         */
+        $colTo->update($criteria2,$criteria2Copy);
+    }
+    
 }
-
 ?>
