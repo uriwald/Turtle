601fd9ea88b428b04aa749d5a39323df87e22a9f
uri wald <uriwald@walla.com>
2013-03-07 09:58:05 +0530

    Adding user login info to loggedinInfo collection

--- files/utils/userUtil.php
+++ files/utils/userUtil.php
@@ -14,17 +14,54 @@
            if ($resultcount == 0)
                return false;
            else
+           {
+               self::user_login_log($username , $db);
+               
                return true;
+           }
      }
+     /*
+      * Putting user login info
+      */
+     private static function user_login_log($username , $db)
+     {
+         $loginTime = date("Y-m-d H:i:s");
+         $loginUser   =   $db->users_login ;
+         //Check if user already exist
+         $userQuery       = array('username' => $username );
+         $resultcount     = $loginUser->count($userQuery);
+         //if exist do
+         if ($resultcount > 0){
+             $criteria = $loginUser->findOne(array('username' => $username ));
+             $loggedinfo    =   $criteria['logged'];
+             $loggedinfo  .= " ," . $loginTime;
+             $result = $loginUser->update($criteria, array('$set' => array("username" => $username, "logged" => $loggedinfo)));
+         }
+         
+         //else do
+         else{
+                $structure = array("username" => $username, "logged" => $loginTime);
+                $result = $loginUser->insert($structure, array('safe' => true));
+         }
+     } 
      
+     /*
+      * Getting the username
+      * if the username equal to Admin then show all lessons
+      */
      public static function showUserLessons($username) 
      {
            $m = new Mongo();
            $db = $m->turtleTestDb;	
            $users = $db->lessons_created_by_guest;
-           
-           $userQuery       = array('username' => $username);
+           if ($username == "lucio")
+           {
+               $results     = $users->find();
+           }
+           else {
+           $userQuery       =  array('username' => $username);
            $results     = $users->find($userQuery);
+           }
            //Case no user found
                return $results;
      }
