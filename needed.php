fdc504adbad574d63499c80f2e4f8bf9cb6096b4
uri wald <uriwald@walla.com>
2013-03-07 16:57:22 +0530

    Sending mail on seperate page

--- needed.php
+++ needed.php
@@ -118,133 +118,44 @@
             <div class="content">
                 <div class="page-header"> 
                     <h1>
-                        <?php  echo _("About the project"); ?> 
+                        <?php  echo _("Help Us"); ?>  
                     </h1>  
                 </div>
                 <div class="row">
                     <div class="span10">
                         <!-- <div id="logo1"></div> -->
                         <h2>
-                            <?php  echo _("About the project"); ?>  
+                            <?php  echo _("Translate"); ?> 
                         </h2>
                         <div class='cleaner_h20'></div>
                         <p>
                             <?php
-                                echo _("The project contains a client side learning environment and a compiler for the <a target='_bjlank' href='http://he.wikipedia.org/wiki/%D7%9C%D7%95%D7%92%D7%95_(%D7%A9%D7%A4%D7%AA_%D7%AA%D7%9B%D7%A0%D7%95%D7%AA)'>Logo</a> Programming language.");
-                                echo _("The project enables to learn the Logo language and programming principles and can be used for programming logo");
+                                echo _("TurtleAcademy aspires to be availabe for kids all around the world. ");
+                                echo _("if you wish to help translating the project please contact us.");
                             ?>
                         </p>    
                         <div id="logo2"></div>
                         <h2>     
                             <?php
-                                echo _("Why was it done");
-                            ?>
+                                echo _("Add some content");
+                            ?> 
                         </h2>
                         <div class='cleaner_h20'></div>
                         <p>
                             <?php
-                                echo _("My brother has been volunteering in a primary school in Israel for the past few years.");
-                                echo _("He was trying to instill the basic programming skills in the children and he discovered that there are three main problems.");
-                                echo _("The first is the language barrier (all the programming languages are using English and the children's do not speak the language ),"); 
-                                echo _("the second issue is that currently there are not many online learning programs which address children.");
-                                echo _("The third issue is the installation requirement of an appropriate programming environment in order to be able to start programming.");
+                                echo _("Did you like the lessons and you feel you can contribute more knowledge. ");
+                                echo _("Please register and create more lesson in your own native language.");
+                                echo _("after adding the lessons they can be avialable for kids all around the world."); 
                             ?>
-                        </p>
- 
-                            <?php
-                                echo _("For this reason we thought it would be a good idea to join forces and create TurtleAcademy");
-                                echo _("The inspiration for the project came from other projects like <a target='_blank' href='http://www.khanacademy.org/'> Khanacademy</a> And a javascript learning project named <a target='_blank' href='http://www.codecademy.com/'>Codecademy</a>");
-
-                            ?>   
-                        <div id="logo3"></div>
-                        <h2>       
-                            <?php
-                                echo _("Who are we ?");
-                            ?>
+                        </p>  
+                        <h2>     
+                            <a href="mailto:support@turtleacademy.com?subject=Helping TurtleAcademy project" target="_blank">Contact Us</a> 
                         </h2>
-                        <div class='cleaner_h20'></div>
-                        <p>
                         
-                            <?php
-                                echo _("Lucio  - Webmaster  and  amateur ping pong player");  echo "</br>";          
-                                echo _("Ofer   - Father of three , amateur entrepreneur");    echo "</br>";             
-                                echo _("Amir   - Colnect master , uncle of six");             echo "</br>";             
-                                echo _("Dana   - Tiger painter , likes to read lessons");     echo "</br>";  
-                                echo _("Ayelet - Full time mother");                          echo "</br>";             
-                                echo _("Almog  - Blogger , made his first QA steps at the age of seven"); echo "</br>";        
-                                echo _("Inbar  - Very talented Wii player");                  echo "</br>";          
-                                echo _("Raz    - QA and vegetarian");                         echo "</br>";
-                                echo _("Yuval  - QA and future Messi");                       echo "</br>";
-                            ?>   
-                        </p>
-                        <div id="logo4"></div>
-                        <h2>  
-                            <?php
-                                echo _("Tricks");
-                            ?>
-                        </h2>
-                        <div class='cleaner_h20'></div>
-                        <p>
-
-                            <?php
-                            echo _("New commands that have been learned will be saved even after the browser restarts"); echo "</br>";
-                            echo _("The turtle remembers the student's progress over time, allowing the student to follow his own progress"); "</br>";
-                            echo _("The drawing that appear below actually show live logo programs !"); echo "</br>";
-                            ?>
-                        </p>
-                        <div id="logo5"></div>
-                            <h2>
-                                <?php
-                                    echo _("Credits");
-                                ?>
-                            </h2>
-                            <div class='cleaner_h20'></div>
-                            <p>
-                                <?php
-                                    echo _("A lot of written libraries facilitate the creation of the website"); echo "</br>";
-                                    echo _("<a target='_blank' href='http://www.calormen.com/Logo/'> Joshua Bell - wrote the logo component </a>"); echo "</br>";
-                                    echo _("<a target='_blank' href='https://github.com/replit/jq-console'> jsconsole- wrote the input element ( Console ) </a>"); echo "</br>";
-                                    echo _("<a target='_blank' href='http://jquery.com'> jquery - component which enabled easy java scripting </a>"); echo "</br>";
-                                    echo _("<a target='_blank' href='http://jqueryui.com/'> jqueryui - a collection of graphical components </a>"); echo "</br>";
-                                    echo _("<a target='_blank' href='http://api.jquery.com/category/plugins/templates/'> jquerytemplates - enable transmission of data to html </a>"); echo "</br>";
-                                ?>  
-                            </p>
-                            <div id="logo6"></div>
-
-                            <h2> 
-                                <?php
-                                    echo _("Contact us");
-                                ?>
-                            </h2>
-                            <div class='cleaner_h20'></div>
-                            <p>
-                                <a href="mailto:support@turtleacademy.com" target="_blank"> <?php echo _("Send an email"); ?> </a>
-                            </p>
                    </div> <!-- end of span10 -->
               </div> <!-- end of row -->              
             </div> <!-- end of content -->
         </div> <!-- End of container --> 
-        <script>
-            function do_logo(id ,cmd) {
-                $('#'+id).css('width', '100px').css('height', '100px').append('<canvas id="'+id+'c" width="100" height="100" style="position: absolute; z-index: 0;"></canvas>' +
-                    '<canvas id="'+id+'t" width="100" height="100" style="position: absolute; z-index: 1;"></canvas>');
-                var canvas_element2 = document.getElementById(id+"c");
-                var turtle_element2 = document.getElementById(id+"t");
-                var turtle2 = new CanvasTurtle(
-                canvas_element2.getContext('2d'),
-                turtle_element2.getContext('2d'),
-                canvas_element2.width, canvas_element2.height);
-
-                g_logo2 = new LogoInterpreter(turtle2, null);
-                g_logo2.run(cmd);
-            } 
-            //do_logo ('logo1', 'fd 20');
-            do_logo ('logo2', 'repeat 8 [fd 10 rt 360/8]');
-            do_logo ('logo3', 'repeat 10 [repeat 8 [fd 10 rt 360/8] rt 360/10]');
-            do_logo ('logo4', 'repeat 10 [fd repcount*8 rt 90] ht');
-            do_logo ('logo5', 'window repeat 10 [fd 3 * repcount repeat 3 [fd 15 rt 360/3] rt 360/10] ht');
-            do_logo ('logo6', 'window pu home repeat 20 [ setlabelheight 20-repcount fd repcount label "HTML5Fest bk repcount rt 18 ] ht');
-        </script>
  <script>
         // Select language in main page
       $(document).ready(function() {
