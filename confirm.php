226d7e7ed178babac0a26fba1c5aa863952dcbec
uri wald <uriwald@walla.com>
2013-03-15 23:10:05 +0530

    Creating a test user in order to bypass al confirmation issues when $istest = true and redirection to login page in case of success

--- confirm.php
+++ confirm.php
@@ -7,22 +7,30 @@ include_once $incDirPath . 'elements/header.php';
 //setup some variables
 $action = array();
 $action['result'] = null;
+$istest =   false;
 
 //check if the $_GET variables are present
 //quick/simple validation 
 if (empty($_GET['email']) || empty($_GET['key'])) {
-    $action['result'] = 'error';
-    $action['text'] = 'We are missing variables. Please double check your email.';
+    if ($istest)
+    {
+        $action['result'] = 'success';
+        $action['text'] = "User has been confirmed. Thank-You! please <a href='" . $rootDir . "registration.php'>" .  _('login')  . "</a>" ; ?> 
+            <?php
+    }
+    else
+    {
+        $action['result'] = 'error';
+        $action['text'] = 'We are missing email address or generated key. Please double check your email.';
+    }
 }
 
-if ($action['result'] != 'error') {
+if ($action['result'] != 'error' && !$istest) {
     $m = new Mongo();
     $db = $m->turtleTestDb;
     $usersconfirmation = $db->users_waiting_approvment;
     $users = $db->users;
     //cleanup the variables
-    //$email = mysql_real_escape_string($_GET['email']);
-    //$key = mysql_real_escape_string($_GET['key']);
     $email = $_GET['email'];
     $key = $_GET['key'];
 
@@ -39,27 +47,14 @@ if ($action['result'] != 'error') {
     $uesrid = $check_key['userid'];
     $confirmid = $check_key['_id'];
     $confirmidMongo = new MongoId($confirmid);
-    //echo " confirm Id is ,, " . $confirmid;
-    //echo $uesrid;
-    // if(mysql_num_rows($check_key) != 0){
     if ($resultcount != 0) {
         //get the confirm info
-        //$confirm_info = mysql_fetch_assoc($check_key);
-        //Won't be use in here
-        //confirm the email and update the users database
-        //$update_users = mysql_query("UPDATE `users` SET `active` = 1 WHERE `id` = '$confirm_info[userid]' LIMIT 1") or die(mysql_error());
         $theObjId = new MongoId($uesrid);
         $criteria = $users->findOne(array("_id" => $theObjId));
         $criteriaUpdate = $criteria;
         $criteriaUpdate['confirm'] = true;
-        //echo " Criteria is : " ;
-        //var_dump($criteria);
-        //echo "Criteria update confirm = " . $criteriaUpdate["confirm"];
-        $update_users = $users->update($criteria, $criteriaUpdate);
-        //
-        //delete the confirm row
-        //$delete = mysql_query("DELETE FROM `confirm` WHERE `id` = '$confirm_info[id]' LIMIT 1") or die(mysql_error());
 
+        $update_users = $users->update($criteria, $criteriaUpdate);
         $result = $usersconfirmation->remove(array('_id' => $confirmidMongo), array("justOne" => true));
         if ($update_users) {
             $action['result'] = 'success';
