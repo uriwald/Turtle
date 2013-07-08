fd23ec8f69afd480b502a28240f3995bf1e24777
uri wald <uriwald@walla.com>
2013-07-08 12:54:30 +0800

    Creating new home Page

--- index.php
+++ index.php
@@ -47,7 +47,7 @@ $currentPage = $parts[count($parts) - 1];
         }
         ?>
         <script src="<?php echo $rootDir; ?>twitter-bootstrap/twitter-bootstrap-v2/docs/assets/js/bootstrap-carousel.js"></script>
-        <link href="<?php echo $rootDir; ?>twitter-bootstrap/twitter-bootstrap-v2/docs/assets/css/bootstrap.css" rel="stylesheet">
+        <!--<link href="<?php echo $rootDir; ?>twitter-bootstrap/twitter-bootstrap-v2/docs/assets/css/bootstrap.css" rel="stylesheet"> -->
         
         
         <script type="application/javascript" src="<?php echo $rootDir; ?>files/js/langSelect.js"></script> <!-- Language select -->                         
@@ -68,6 +68,9 @@ $currentPage = $parts[count($parts) - 1];
         <link rel='stylesheet' href='<?php echo $rootDir; ?>files/css/topbar.css' type='text/css' media='all'/> 
         <link rel='stylesheet' href='<?php echo $rootDir; ?>files/css/footer.css' type='text/css' media='all'/> 
         <link rel='stylesheet' href='<?php echo $rootDir; ?>files/css/zocial.css' type='text/css' media='all'/>
+        
+        <script type="application/javascript" src="<?php echo $rootDir; ?>files/logo.js"></script> <!-- Logo interpreter -->
+        <script type="application/javascript" src="<?php echo $rootDir; ?>files/turtle.js"></script> <!-- Canvas turtle -->
         <?php
             cssUtils::loadcss($locale, $rootDir . "files/css/interface");
             cssUtils::loadcss($locale, $rootDir . "files/css/doc");
@@ -112,20 +115,52 @@ $currentPage = $parts[count($parts) - 1];
         $language['he'] = "he"; 
 
         $topbar->printTopBar($rootDir, $class, $login, $topbarDisplay, $languagesDisplay
-                , $signUpDisplay, $language, $empty = "");
+                , $signUpDisplay, $language, $empty = "" );
         ?>
         <div class="container">
             <!-- Main hero unit for a primary marketing message or call to action -->
             <div class="hero-unit" id="hero-top">
                 <!--
-                <div style="height:80px;"> <p style="float:left;"><img class='pull-right' src='files/turtles.png' alt=''/></p> </div>
+                <img class='pull-right' src='files/turtles.png' alt=''/>
                 -->
-                
-                <h1><?php echo _("Turtle Academy") ; ?></h1>
-                <h2 id="h2top"> the easy way to learn programming</h2>
-                <p>Before People got board of start programming in Turtle academy you will see immidiate result for every action . </p>
-                <p>No need to download a software just follow the lesson and start enjoying programming</p>
-                <p><a class="btn primary large" href ="learn.php">Free Lessons </a> <a class="btn primary large">Sign In</a></p>
+              
+                <h1 id="hero-main-title"><?php echo _("Turtle Academy") ; ?></h1> 
+                <h2 id="hero-secondary-title"> the easy way to learn programming</h2>
+                <p class="top">With Turtle Academy is really easy to start creating amazing shapes using LOGO language</p>
+                <p class="top">Here are some examples how programming can be easy and fun</p>
+                <div class='span14'>
+                    <div class="span4" >
+                        <p class="example-title"> Create a square </p>
+                        <p class="example-code"> for [i 10 100 10]     [fd :i rt 90] </p>
+                        <div id="logo2"></div>
+                        
+                        <h2> <!--<img src='images/sample/icon1.png' alt=''/> --></h2>
+
+                    </div>
+                    <div class="span4" >
+                        <p class="example-title"> Cool flower </p>
+                        <p class="example-code"> repeat 8 [rt 45 repeat 6 [repeat 90 [fd 1 rt 2] rt 90]] ht</p>
+                        <div id="logo3"></div>
+                        
+                        <h2> <!--<img src='images/sample/icon1.png' alt=''/> --></h2>
+
+                    </div>
+                    <div class="span4" >
+                        <p class="example-title""> Crazy octagon</p>
+                        <p class="example-code"> cs repeat 36 [ rt 10 repeat 8 [ fd 25 lt 45]] ht</p>
+                        <div id="logo4"></div>
+                        
+                        <h2> <!--<img src='images/sample/icon1.png' alt=''/> --></h2>
+
+                    </div>
+                </div>
+                <div id="start-logo" align="center">
+                    <p class="top">Let's start having fun programming </p>
+                    <div>
+                        <p class="top"><a class="btn primary large" href ="learn.php">Free Lessons </a> <a class="btn primary large" href ="registration.php">Sign In</a></p>
+
+                    </div>
+                </div>
             </div>
             <div id="myCarousel" class="carousel slide">
                 <!--
@@ -136,27 +171,26 @@ $currentPage = $parts[count($parts) - 1];
             </ol>
             <!-- Carousel items -->
 
-                <div class="carousel-inner">
+                <div class="carousel-inner" id="crousel-quote">
                     <div class="active item">
                         <div class="carousel"> 
-                                <h1 id="h1quote"> "I got the passion for programming thanks to Turtle Academy"</h1>
-                                <h3> Jin Sin Lin - China</h3>   
+                                <h1 id="running-quote"> "I got the passion for programming thanks to Turtle Academy"</h1>
+                                <h3 id="name-quote"> Jin Sin Lin - China</h3>   
                         </div>        
                     </div>
                     <div class="item">
                         <div class="carousel"> 
-                                <h1 id="h1quote"> "I want to do Mona"</h1>
-                                <h3> Lucio - Israel</h3> 
+                                <h1 id="running-quote"> "I want to do Mona"</h1>
+                                <h3 id="name-quote"> Lucio - Israel</h3> 
                         </div>
                     </div>
                     <div class="item">
                         <div class="carousel"> 
-                                <h1 id="h1quote"> "Burburi change my life"</h1>
-                                <h3> Jin Sin Lin - China</h3>   
+                                <h1 id="running-quote"> "Burburi change my life"</h1>
+                                <h3 id="name-quote"> Jin Sin Lin - China</h3>   
                         </div>
                     </div>
                 </div>
-
             </div>
             <div class="">
                 
@@ -164,27 +198,52 @@ $currentPage = $parts[count($parts) - 1];
 
                 
             <!-- Example row of columns --> 
-            <div class="row"> 
+            <div class="row span16" id="row-info"> 
+                <div class="span1" style="width:30px">
+                    <h2> <!--<img src='images/sample/icon1.png' alt=''/> --></h2>
+                    <p></p>
+                </div>
                 <div class="span5">
                     <h2>Project goal <!--<img src='images/sample/icon1.png' alt=''/> --></h2>
-                    <p>Our objective is teaching people around the world the programming basics in a fun way , everyone can start programming in his mother tongue </p>
+                    <p>Our objective is to teach the programming principles in a fun and easy way and to bring programming to every kid in the world. </p>
+                </div>
+                <div class="span2">
+                    <h2> <!--<img src='images/sample/icon1.png' alt=''/> --></h2>
+                    <p></p>
                 </div>
                 <div class="span5">
                     <h2>Help needed <!--<img src='images/sample/icon2.png' alt=''/>--> </h2>
-                    <p>In order to let people around the world the opportunity to learn programming we need volunteers who are willing to translate the website to more languages</p>
-                    <p><a class="btn" href="#">View details &raquo;</a></p>
+                    <p> In order to make programming approachable for more people  we need some volunteers who are willing to translate the site to their on native language</p>
+                    <!--<p><a class="btn" href="#">View details &raquo;</a></p> -->
                 </div>
             </div>
 
-
+ 
 
         </div> <!-- /container -->
         <?php echo $footer; ?>
     </div> <!-- /main -->
     <script> 
+            function do_logo(id ,cmd) {
+                $('#'+id).css('width', '200px').css('height', '200px').append('<canvas id="'+id+'c" width="200" height="200" style="position: absolute; z-index: 0;"></canvas>' +
+                    '<canvas id="'+id+'t" width="200" height="200" style="position: absolute; z-index: 1;"></canvas>');
+                var canvas_element2 = document.getElementById(id+"c");
+                var turtle_element2 = document.getElementById(id+"t");
+                var turtle2 = new CanvasTurtle(
+                canvas_element2.getContext('2d'),
+                turtle_element2.getContext('2d'),
+                canvas_element2.width, canvas_element2.height);
+
+                g_logo2 = new LogoInterpreter(turtle2, null);
+                g_logo2.run(cmd);
+            } 
+            //do_logo ('logo1', 'fd 20');
+            do_logo ('logo2', 'for [i 10 100 10] [fd :i rt 90]');
+            do_logo ('logo3', 'cs repeat 8 [rt 45 repeat 6 [repeat 90 [fd 1 rt 2] rt 90]] ht');
+            do_logo ('logo4', 'cs repeat 36 [ rt 10 repeat 8 [ fd 25 lt 45]] ht');
 // Select language in main page
     $(document).ready(function() {
-          selectLanguage("<?php echo $_SESSION['locale']; ?>" ,  "<?php echo $rootDir; ?>language/", "index.php" ,"en_US" ); 
+          selectLanguage("<?php echo $_SESSION['locale']; ?>" ,  "<?php echo $rootDir; ?>language/", "index.php" ,"en" ); 
             $('.carousel').carousel({
                  interval: 5000 
             })
