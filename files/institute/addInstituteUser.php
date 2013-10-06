93df8499e63dcdd9eb735ea84282d4a6728d620c
uri wald <uriwald@walla.com>
2013-10-06 15:23:20 +0800

    Give the institute the ability to add a student

---
+++ files/institute/addInstituteUser.php
@@ -0,0 +1,244 @@
+<!DOCTYPE html>
+<?php
+if (session_id() == '')
+    session_start();
+$phpDirPath = "../registration/inc/php/";
+include_once $phpDirPath . 'config.php';
+include_once $phpDirPath . 'functions.php';
+require_once ('../../environment.php');
+require_once ("../../localization.php"); 
+require_once ("../cssUtils.php");
+require_once ('../utils/topbarUtil.php');
+?>
+<html dir="<?php echo $dir ?>" lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Add Student</title>
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <?php
+
+        require_once("../utils/includeCssAndJsFiles.php");
+        ?>     
+        <link rel='stylesheet' href='<?php echo $rootDir; ?>files/css/zocial.css' type='text/css' media='all'/>   
+        <script src="<?php echo $rootDir; ?>ajax/libs/jquery/validator/dist/jquery.validate.js" type="text/javascript"></script>
+        <script type='text/javascript'> 
+            $(document).ready(function(){
+                var gt = new Gettext({'domain' : 'messages'});
+                $('#topbar').dropdown();
+                $('#username_in').focus();
+                
+                $("#sign-up-form").validate({
+                    rules: {
+                        username: {
+                            required: true,
+                            minlength: 4
+                        },
+                        password: {
+                            required: true,
+                            minlength: 5
+                        }
+                    },
+                    messages: {
+                        username: {
+                            required: gt.gettext("Please enter your username"), 
+                            minlength: gt.gettext("Your username must contain at least 4 characters")
+                        },
+                        password: {
+                            required: gt.gettext("Please enter your password"),
+                            minlength: gt.gettext("Your password must contain at least 5 characters")
+                        },
+                        email: gt.gettext("Please enter a valid email address")
+                    }
+                }); 
+                try {
+                    var pages = $("#selectedLanguage").msDropdown({on:{change:function(data, ui) {
+                                var val = data.value;
+                                $.Storage.set("locale",val);
+                                if(val!="")
+                                {
+                                    window.location.assign('registration.php?l=' + val);
+                                
+                                }
+                                //window.location = 'registration.php?l=' + val ;
+                            }}}).data("dd");
+                    var pageIndex   =  $.Storage.get("locale");
+                    if (pageIndex == "")
+                        pageIndex   = "en_US";
+                    pages.setIndexByValue(pageIndex);
+                } catch(e) {
+                    console.log(e);	
+                }
+                $('#termsofuse').each(function() {
+                    var locale      = $.Storage.get('locale');      
+                    if (locale != "he_IL")
+                        var $dialog = $('<div dir="ltr"></div>');
+                    else
+                        var $dialog = $('<div dir="rtl"></div>');
+                    var $link = $(this).one('click', function() {                      
+                        $dialog                           
+                        .load('termsOfUse.php')
+                        .dialog({
+                            title: "Terms of use",
+                            width: 700,
+                            close: function( event, ui ) {
+                            }
+
+                        }); 
+                        $link.click(function() {
+                            $dialog.dialog('open');
+                            return false;
+                        });
+                        return false;                       
+                    });                                          
+                });                       
+            });
+ 
+            $(document).delegate('.switch', 'click', function(){
+
+                var c = $(this).attr('data-switch');
+                $('#sign-in-form').slideUp(300, function(){ $(this).addClass('hide'); });
+                $('#forgot-password-form').slideUp(300, function(){ 
+                    $(this).addClass('hide'); 
+                });
+                $('#'+c).slideDown(300, function(){
+                    $(this).removeClass('hide');
+                    $('input:first', this).focus();
+                });
+                c = null;
+            });
+
+        </script>
+
+    </head>
+    <body>
+<?php
+function addUserToDb($username, $password, $users, $db) {
+    $date = date('Y-m-d H:i:s');
+    $userStructure = array("username" => $username, "password" => $password,"badges" => "", "email" => "",
+        "confirm" => true, "institute_email" => $_SESSION['institute_email'] , "date" => $date);
+    $userResult = $users->insert($userStructure, array('safe' => true));
+}
+ if (!isset($_SESSION['institute_email']))
+ {
+     echo " You don't have institute admin permission Please contact site administrator" ;
+ }
+ else
+ {
+        //Strings for Password reset
+    $strContactSupoort = _("please contact the TurtleAcademy support");
+//In case the user properly inserted into the database
+    $strWelcomeMsg   = _("Welcome to TurtleAcademy");
+//String for checking Email and username validation
+
+    $strUserNExist  = _("Username is already exist in the system");
+    $strChooseNewUN = _("please choose another username");
+
+//setup some variables/arrays
+        $action = array();
+        $action['result'] = null;
+        $text = array();
+
+        //If a new user is want to signup
+        if (isset($_POST['signup'])) {
+            $isTestUser = false;
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+            //quick/simple validation
+            if (empty($username)) {
+                $action['result'] = 'error';
+                array_push($text, 'You forgot your username');
+            }
+            if (empty($password)) {
+                $action['result'] = 'error';
+                array_push($text, 'You forgot your password');
+            }
+
+            if ($action['result'] != 'error') {
+                $password = md5($password);
+                $m = new Mongo();
+                $db = $m->turtleTestDb; 
+                $users = $db->users;
+
+                //Check if username is already taken
+                $queryUsername = array('username' => $username);
+                $existUsername = $users->count($queryUsername);
+
+
+                if ($isTestUser) { //Case of testing we will insert to db
+                    addUserToDb($username, $password, $users, $db);
+                } else if ($existUsername > 0) { //Check if email already exist
+                    $action['result'] = 'error';
+                    array_push($text, $strUserNExist . " " . $strChooseNewUN);
+                } else {
+                    addUserToDb($username, $password, $users, $db);
+                    $action['result'] = 'info';
+                    array_push($text, "New User - " . $username . " has been added" );
+                }
+            }
+            $action['text'] = $text;
+        }
+        //Printing the topbar menu
+        topbarUtil::printTopBar("registration");
+        ?>
+
+
+
+        <div class="container">
+            <div class='row'>
+                <!-- Main hero unit for a primary marketing message or call to action -->
+                <div class="well span6 offset2">
+                    <form class='form-stacked' id='sign-up-form' method="post" action="">
+                        <h2><?php echo _("Hello"); echo " " ; echo $_SESSION['institute_email']; ?></h2>
+                        <h2><?php echo _("Add Student"); ?></h2>
+                        <?php
+                        echo show_errors($action);
+                        ?>     
+                        <div class="clearfix" lang="<?php echo $dir ?>">
+                            <label for="username_up" id="signUpUserNameLbl"><?php echo _("Username"); ?></label>
+                            <div class="input" lang="<?php echo $dir ?>">
+                                <input id="username" name="username" size="30" type="text" class='xlarge'/>
+                                </br>
+                            </div>
+                        </div>         
+                        <div class="clearfix" lang="<?php echo $dir ?>">
+                            <label for="pwd_up" id="signUpUserNameLPwdLbl"><?php echo _("Password"); ?></label>
+                            <div class="input" lang="<?php echo $dir ?>">
+                                <input id="password" name="password" size="30" type="password" class='xlarge'/>
+                            </div>
+                        </div>        
+                        <div class='cleaner_h10'></div>           
+                        <ul class="inputs-list">
+                            <li>
+                                <label class="checkCondition" lang="<?php echo $dir ?>">
+                                    <input type="checkbox" name="terms_up" id='terms_up' value="yes" checked='true' />
+                                    <span for='terms_up' id="signupAgreeToTerms"><?php echo _("Agree to"); ?> <a id="termsofuse" href='#'><?php echo _("Terms of Use"); ?></a></span>
+                                </label>
+                            </li>
+                        </ul>       
+                        <div class='cleaner_h10'></div>
+                        <input type='submit' value='<?php echo _("Add student"); ?>&raquo;' id='signup' name='signup' class="btn primary"/>
+                    </form>
+                </div>    
+              
+            </div>
+            <div class='row span12 offset120'>
+                <p id='contact_us'>
+                    <?php echo _("Having some problems to register");echo "? " ;echo _("please"); ?>
+                    <a href="mailto:support@turtleacademy.com" target="_blank"> <?php echo _("Contact Us"); ?> </a>
+                </p> 
+            </div> 
+            <div class='cleaner'></div>
+
+            <footer style='text-align:center;'>
+                <p>&copy; <?php echo _("TurtleAcademy"); ?></p>
+            </footer>
+
+        </div> <!-- /container -->
+
+    </body>
+</html>
+
+<?php
+ } // Ending else condition if enabling only institute admin to add users
+?> 
