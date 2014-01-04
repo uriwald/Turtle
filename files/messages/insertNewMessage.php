2ff4c69b46a5d6bbde0cebe6b7b23db98fe5c52b
uri wald <uriwald@walla.com>
2014-01-04 22:34:45 +0800

    Adding a messages  system, For now the system can send a message to a user , mark it as read or unread , Users are able to send messages to other users , an indication will be shown when a logged in User has a new message

---
+++ files/messages/insertNewMessage.php
@@ -0,0 +1,12 @@
+<html>
+    <body>
+        <h4>Translate strings</h4>
+        <form action="saveNewsMessage.php" method="post"> 
+            Send From : <input name="sendfrom" type="text" /> </br>
+            Send To   : <input name="sendto" type="text"  /> </br>
+            Subject   : <input name="subject" type="text" >  </input> </br>
+            Content   : <textarea name="context" rows="6" cols="60"></textarea> </br>       
+            <input type="submit" />
+        </form>      
+    </body>
+</html>
