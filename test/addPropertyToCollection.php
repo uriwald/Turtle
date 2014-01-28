f194d509623d53edbbb5df2c24d54605ae9ba0ca
uri wald <uriwald@walla.com>
2014-01-28 20:13:37 +0800

    Saving program image and then under "users programs" loading the image and not running the javascript code

--- test/addPropertyToCollection.php
+++ test/addPropertyToCollection.php
@@ -1,4 +1,10 @@
 <?php
 require_once '../files/utils/collectionUtil.php';
-collectionUtil :: addPropertyToAllCollectionObjects("stringTranslation" , "pagecode" , "1"); 
+collectionUtil :: addPropertyToAllCollectionObjects("programs" , "comments" , ""); 
+collectionUtil :: addPropertyToAllCollectionObjects("programs" , "numOfComments" , "0"); 
+collectionUtil :: addPropertyToAllCollectionObjects("programs" , "displayInProgramPage" , false); 
+collectionUtil :: addPropertyToAllCollectionObjects("programs" , "precedence" , "99"); 
+collectionUtil :: addPropertyToAllCollectionObjects("programs" , "img" , ""); 
+//collectionUtil :: addPropertyToAllCollectionObjects("programs" , "programLocale" , "en_US"); 
+
 ?>
